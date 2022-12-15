8ba93b235ac6a33dd911b61267209a3ea3955895
Szymon Lepianka <slepianka@wp.pl>
2022-12-15 21:29:32 +0100

    improves getting cookies (AccessToken)

--- oauth_server_front/src/middleware/session.jsx
+++ oauth_server_front/src/middleware/session.jsx
@@ -1,12 +1,14 @@
-import Cookies from "js-cookie";
+import { Cookies } from "react-cookie";
 
 export const getSessionCookie = () => {
-  const sessionCookie1 = Cookies.get("AccessToken1");
-  const sessionCookie2 = Cookies.get("AccessToken2");
+  const cookieManager = new Cookies();
+  const cookies = cookieManager.getAll();
 
-  if (sessionCookie1 === undefined && sessionCookie2 === undefined) {
-    return "";
-  } else {
-    return sessionCookie1 ? sessionCookie1 : sessionCookie2;
+  for (const cookie in cookies) {
+    if (cookie.startsWith("AccessToken")) {
+      // TODO call validate AccessToken
+      return cookies[cookie];
+    }
   }
+  return "";
 };
